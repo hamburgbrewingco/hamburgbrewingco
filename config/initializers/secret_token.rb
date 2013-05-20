104ad9e2ecc73bf5f3cb0f832a62940424cf8522
info@vergetone.com <mike@vergetone.com>
2013-05-19 21:07:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hamburgbrewingco::Application.config.secret_token = '1423b0bef927e35b528e542316540b3397eb4df5ee457ae4342c9949df12f347cf297ef9171510e0d4311b03710854b7229ed9d807fcf6eab80ab7a5daa7bb73'
